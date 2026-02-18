4adae47d622f9665860e9808d2750a5f9eab5434
Noah Mühl <noahmuehl@me.com>
2026-02-18 17:40:37 +0100

    v0.1.13: - add optional setting to auto-pause music/audio while transcribing speech (@JsBergbau) - fix bug that caused recording after quitting keyboard, when auto-start mic was enabled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.notune.transcribe"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.1.12"
+        versionCode = 14
+        versionName = "0.1.13"
         ndk {
             abiFilters += "arm64-v8a"
         }
